27e15456fe6af109ca58abc1c5a59aa9b330ccf2
IJakePL <burzynsonek@gmail.com>
2025-06-01 10:10:33 +0200

    Usunięcie nieużywanych plików CSS i widoków oraz aktualizacja linków w stopkach

--- routes/index.js
+++ routes/index.js
@@ -6,14 +6,6 @@ router.get("/", async (req, res) => {
   res.render("index", {});
 });
 
-router.get("/privacy-policy", async (req, res) => {
-  res.render("privacy-policy", {});
-});
-
-router.get("/terms-of-service", async (req, res) => {
-  res.render("terms-of-service", {});
-});
-
 router.get("/support", async (req, res) => {
   res.render("support", {});
 });
